5c82b87590ba22214c6ec2b04fec55ace00eb80a
obedgodfrey <obedmarcelcomedian@gmail.com>
2024-03-08 21:53:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var prevScrollPos = window.pageYOffset
+var header1Height = document.querySelector('.header-1').offsetHeight
+var header2 = document.querySelector('.header-2')
+
+window.addEventListener('scroll', function () {
+  var currentScrollPos = window.pageYOffset
+
+  if (currentScrollPos > header1Height) {
+    header2.style.top = '0' // Show the second header
+    header2.classList.remove('hide-content') // Show the content
+  } else {
+    header2.style.top = '-20vh' // Hide the second header
+    header2.classList.add('hide-content') // Hide the content
+  }
+
+  prevScrollPos = currentScrollPos
+})
